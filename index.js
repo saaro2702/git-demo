3ddfab960db94aa5fab53385c9f8e299c38279e0
Saar Ohayon <saaro2702@gmail.com>
2026-04-29 18:31:12 +0300

    test

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Shulam!!!!!!!!!!!!@#!@');
+import log from "@ajar/marker";
+
+
+log.blue ('Shulam!!!!!!!!!!!!@#!@');
+
+
+export function greet(name) {
+ log.green(`Hello, ${name}!`);
+}
